d6aeb6e2451a55a181ec4162f964099b7d57a83c
Charles Stoeter <charlesstoeter@gmail.com>
2025-06-09 16:25:36 -0400

    updated sprite.h file

--- CharlesStoeterGameDesignLab10/Sprite.h
+++ CharlesStoeterGameDesignLab10/Sprite.h
@@ -7,8 +7,16 @@ public:
 	void bouncesprite(int SCREEN_W, int SCREEN_H);
 	void load_animated_sprite(int size);
 	void drawSprite();
-	int getX() { return x; }
-	int getY() { return y; }
+	int getX() { 
+		return x; 
+	}
+
+
+	int getY() {
+		return y; 
+	}
+
+	void assignRandomSpecialty(); // Assign one random behavior 
 
 
 private:
@@ -20,4 +28,18 @@ private:
 	int curframe, maxframe, animdir;
 	int framecount, framedelay;
 	ALLEGRO_BITMAP* image[9];
+
+	bool CollisionIsTrue = false;
+	bool SpinningSprite = false;
+	bool ScaredSprite = false;
+	bool BabySprite = false;
+	bool FreezeSprite = false;
+
+	float lastCollisionTime = 0; // time of last collision
+	float scaleFactor = 1.0f;  // for BabySprite shrinking
+	float freezeStartTime = 0;  // for FreezeSprite freeze delay
+
+
+
+
 };
